b77650ad99ff9775a533893cc67f60caf4391416
Fhjorth <43441370+Fhjorth@users.noreply.github.com>
2021-04-03 23:54:03 +0200

    first commit
    Signed-off-by: Fhjorth <43441370+Fhjorth@users.noreply.github.com>
    
    	new file:   OneProject/Info.plist

---
+++ OneProject/OneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneProjectApp.swift
+//  OneProject
+//
+//  Created by Frederik Hjorth on 03/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct OneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
