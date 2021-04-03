ea47cf7adee02deb32680268ec24597bf81c5b41
Fhjorth <43441370+Fhjorth@users.noreply.github.com>
2021-04-04 00:34:41 +0200

    Colors

---
+++ OneProject/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  OneProject
+//
+//  Created by Frederik Hjorth on 04/04/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let primary = Color("Primary")
+    static let secondary = Color("Secondary")
+    static let inactive = Color("inactive")
+    static let background = Color("Background")
+}
